cb36d1cba3278e37f092ef3af6157c3fe2a354d9
christiantc <christiantambo15@gmail.com>
2019-08-17 17:57:03 -0400

    uva 272

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main(){
 
     string a;
-    cin>>a;
+    getline(cin,a);
     bool aux=false;
     for(int i=0;i<a.size();i++)
     {
