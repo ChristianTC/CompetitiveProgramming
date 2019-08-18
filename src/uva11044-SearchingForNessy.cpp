cc7dd9968c67b522934ce8553d472f76dfda10d6
christiantc <christiantambo15@gmail.com>
2019-08-18 19:31:48 -0400

    uva11044 Searching for Nessy

---
+++ src/uva11044-SearchingForNessy.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,a,b;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        cout<<(a/3)*(b/3)<<endl;
+    }
+    return 0;
+}
